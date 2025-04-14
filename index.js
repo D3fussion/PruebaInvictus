19d5cf9f1761ee5c51277c4ed0003b64c6e1c56d
D3fussionC0ld <diego.quijada03@gmail.com>
2025-04-14 07:11:21 -0700

    Hola

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-// IMPORTACIONES
 import express from 'express';
 import dotenv from 'dotenv';
-import conectarDB from './config/db.js';
+import conectarDB from '../config/db.js';
 
-// RUTAS PARA LA API
-import reactivoRoutes from './routes/reactivoRoutes.js';
-import inicioRoutes from './routes/inicioRoutes.js';
-import notificacionRoutes from './routes/notificacionRoutes.js';
+import reactivoRoutes from '../routes/reactivoRoutes.js';
+import inicioRoutes from '../routes/inicioRoutes.js';
+import notificacionRoutes from '../routes/notificacionRoutes.js';
+
+import { createServer } from '@vercel/node';
+import { parse } from 'url';
+
+dotenv.config();
+conectarDB();
 
 const app = express();
 app.use(express.json());
-
-dotenv.config();
-
-conectarDB();
 
 app.use("/api/reactivos", reactivoRoutes);
 app.use("/api/inicio", inicioRoutes);
 app.use("/api/notificaciones", notificacionRoutes);
 
-
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-    console.log(`Servidor funcionando en el puerto ${PORT}`);
-});
+export default app;
