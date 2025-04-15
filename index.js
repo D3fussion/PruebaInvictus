97122c8b4030d21557b67844e67242325d76e9f6
JUAN DIEGO QUIJADA CASTILLO <128547155+D3fussion@users.noreply.github.com>
2025-04-14 17:21:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/reactivos", reactivoRoutes);
-app.use("/", inicioRoutes);
+app.use("/api/inicio", inicioRoutes);
 // app.use("/api/notificaciones", notificacionRoutes);
 
 export default app;
